f8c89b87c6a147d7345770161e5d0d82ff0d4d7d
Atul Kamble <atul_kamble@hotmail.com>
2025-07-30 07:24:00 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "jenkins_public_ip" {
-  description = "Public IP of the Jenkins EC2 instance"
-  value       = aws_instance.jenkins_ec2.public_ip
+  description = "EC2 Public IP"
+  value       = aws_instance.jenkins.public_ip
 }
 
 output "ssh_connection_command" {
-  description = "SSH command to connect to the Jenkins EC2 instance"
-  value       = "ssh -i jenkins.pem ec2-user@${aws_instance.jenkins_ec2.public_ip}"
+  description = "SSH into EC2"
+  value       = "ssh -i jenkins.pem ec2-user@${aws_instance.jenkins.public_ip}"
 }
